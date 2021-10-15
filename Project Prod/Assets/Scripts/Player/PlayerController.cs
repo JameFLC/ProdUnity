66f38232438212d315656e2014324ede4b505f53
Jame_FLC <jame.flochlecarour@gmail.com>
2021-10-15 10:31:35 +0200

    Added proto player controller

--- Project Prod/Assets/Scripts/Player/PlayerController.cs
+++ Project Prod/Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Variables
+    [SerializeField]
+    private GameObject playerCamera;
+
+
+    private Rigidbody RB;
+
+    private Vector3 speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        RB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        speed.x = Input.GetAxis("Horizontal");
+        speed.z = Input.GetAxis("Vertical");
 
+    }
+    private void FixedUpdate()
+    {
+        RB.MovePosition(new Vector3(speed.x, 0, speed.z));
     }
 }
